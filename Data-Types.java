b22056a611720d6ddae3094d7327c8c7a991ba88
SunnyKumar Jonwal <106869482+SunnyKumar-Jonwal@users.noreply.github.com>
2025-04-19 11:25:08 +0530

    Add files via upload

---
+++ Data-Types.java
@@ -0,0 +1,52 @@
+/*
+
+1. What Is Data Type?
+
+-> Data Type Is A Classification Of Data That Specifies The Type Of Data That Can Be Stored In A Variable.
+
+-> Data Types Are Used To Specify The Type Of Data That Can Be Stored In A Variable.
+
+-> Data Types Are Used To Specify The Type Of Data That Can Be Stored In A Variable.
+
+-> In Java, There Are Two Types Of Data Types:
+-> Primitive Data Types
+-> Non-Primitive Data Types
+
+2. What Is Primitive Data Type?
+
+-> Primitive Data Types Are The Most Basic Data Types In Java.
+
+-> Primitive Data Types Are Given Below:
+-> byte : 8-bit signed integer, range: -128 to 127, Used to save memory in large arrays.
+
+-> short : 16-bit signed integer, range: -32,768 to 32,767, Used to save memory in large arrays.
+
+-> int : 32-bit signed integer, range: -2^31 to 2^31-1, Used to save memory in large arrays.
+
+-> long : 64-bit signed integer, range: -2^63 to 2^63-1, Used to save memory in large arrays.
+
+-> float : 32-bit floating point, range: -3.40282347E+38 to 3.40282347E+38, Used to save memory in large arrays.
+
+-> double : 64-bit floating point, range: -1.79769313486231570E+308 to 1.79769313486231570E+308, Used to save memory in large arrays.
+
+-> char : 16-bit Unicode character, range: '\u0000' to '\uffff', Used to save memory in large arrays.
+
+-> boolean : 1-bit boolean value, range: true or false, Used to save memory in large arrays.
+
+2. What Is Non-Primitive Data Type?
+
+-> Non-Primitive Data Types Are The Most Basic Data Types In Java.
+
+-> Non-Primitive Data Types Are Given Below:
+
+-> String : A sequence of characters, range: any character, Used to save memory in large arrays.
+
+-> Array : A collection of elements of the same type, range: any element, Used to save memory in large arrays.
+
+-> List : A collection of elements of the same type, range: any element, Used to save memory in large arrays.
+
+-> Set : A collection of elements of the same type, range: any element, Used to save memory in large arrays.
+
+-> Stack : A collection of elements of the same type, range: any element, Used to save memory in large arrays.
+
+ */
